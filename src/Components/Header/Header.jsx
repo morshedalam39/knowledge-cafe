23cfb7d63ecbac7ab63053c16e4fcd14d2da1369
morshedalam39 <morshedalam4239@gmail.com>
2023-04-01 15:24:18 +0600

    chaged heder icon

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
         <div className="dropdown dropdown-end">
           <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
             <div className="w-10 rounded-full">
-            <img src="../../../public/145867.png" />
+            <img src="145867.png" />
             </div>
           </label>
 
